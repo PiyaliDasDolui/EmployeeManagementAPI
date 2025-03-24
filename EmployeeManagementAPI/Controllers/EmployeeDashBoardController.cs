e9f28a7d84f86135d32075edf80bac2249c13514
piyalidas.gcetts@gmail.com <piyalidas.gcetts@gmail.com>
2025-03-24 17:53:53 +0530

    Added employee data and controller

---
+++ EmployeeManagementAPI/Controllers/EmployeeDashBoardController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System;
+using EmployeeManagementAPI.Models;
+
+namespace EmployeeManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeDashBoardController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public EmployeeDashBoardController(IWebHostEnvironment env)
+        {
+            _env = env;   
+        }
+        [HttpGet(Name = "GetAllEmployee")]
+        public async Task<IActionResult>  GetAllEmployee()
+        {
+            var filePath = Path.Combine(_env.ContentRootPath, "wwwroot", "employeedata.json");
+            if (!System.IO.File.Exists(filePath))
+                return NotFound("Not found file path");
+
+            var json = await System.IO.File.ReadAllTextAsync(filePath);
+
+            var employees = JsonSerializer.Deserialize<List<employeeData>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            return Ok(employees);
+        }
+    }
+}
---
+++ EmployeeManagementAPI/Models/employeeData.cs
@@ -0,0 +1,16 @@
+﻿namespace EmployeeManagementAPI.Models
+{
+    public class employeeData
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public string Designation { get; set; }
+        public int Salary { get; set; }
+        public string DateOfJoining { get; set; }
+        public string Status { get; set; }
+
+    }
+}
