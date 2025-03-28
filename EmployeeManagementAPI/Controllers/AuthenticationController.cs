47aebd716bfdeb5f2668f7dc9694f65426ae4d9d
piyalidas.gcetts@gmail.com <piyalidas.gcetts@gmail.com>
2025-03-28 17:29:28 +0530

    Implemented JWT token authentication

---
+++ EmployeeManagementAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,44 @@
+﻿using EmployeeManagementAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace EmployeeManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost(Name ="login")]
+        public IActionResult Login([FromBody] LoginModel login)
+        {
+            if (login.UserName == "admin" && login.Password == "admin") 
+            {
+                var token = GenerateToken();
+                return Ok(new { token });
+            }
+            return Unauthorized();
+        }
+
+        private string GenerateToken()
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name,"admin")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Employee_portal_secret_key_here."));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "EmployeeManagementPortal",
+                audience: "EmployeeManagementPortal",
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: creds);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- EmployeeManagementAPI/Program.cs
+++ EmployeeManagementAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "EmployeeManagementPortal",
+            ValidAudience = "EmployeeManagementPortal",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("myScerect"))
+        };
+    });
+builder.Services.AddAuthorization();
 var allowdOrigins = builder.Configuration.GetValue<string>("AllowOrigins")!.Split(",");
 builder.Services.AddCors(options =>
 {
@@ -26,6 +44,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCors();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
